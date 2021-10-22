f45717c60a37523e98f36010705ce2562dec70a9
isabeela <isabeelabg@gmail.com>
2021-10-22 12:56:21 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
-
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
