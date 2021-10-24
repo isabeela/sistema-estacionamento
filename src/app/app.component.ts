4ffcd6089a0717463597047e9d721f42cb8fe9b1
isabeela <isabeelabg@gmail.com>
2021-10-24 15:24:49 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent  { 
   name = 'Angular ' + VERSION.major;
 }
